1176b9ef2c76f74daa31005dba87a8f4ae0cb684
Suéllen Castro <castrosuellenx@gmail.com>
2022-09-15 18:28:18 -0300

    add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import {StatusBar} from 'react-native';
 import Home from './src/screens/Home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="#131016"
+      />
+      <Home />
+    </>
+  );
 }
